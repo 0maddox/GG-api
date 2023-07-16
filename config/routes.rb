8d84b8dd0217d309e9c33febae3e61da8925b107
nicholas <nickkiim7@gmail.com>
2023-07-16 23:15:42 +0300

    added migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
-  get 'private/test'
-  get 'private/auth_params'
-  devise_for :users, 
-    path: '', 
-    path_names: {
-      sign_in: 'login',
-      sign_out: 'logout',
-      registration: 'signup'
-    },
-    controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-    }
-  namespace :api do
-    namespace :v1 do
-      resources :cars
-      resources :reservations
-    end
-  end
+  # mount Rswag::Ui::Engine => '/api-docs'
+  # mount Rswag::Api::Engine => '/api-docs'
+  # get 'private/test'
+  # get 'private/auth_params'
+  # devise_for :users, 
+  #   path: '', 
+  #   path_names: {
+  #     sign_in: 'login',
+  #     sign_out: 'logout',
+  #     registration: 'signup'
+  #   },
+  #   controllers: {
+  #     sessions: 'users/sessions',
+  #     registrations: 'users/registrations'
+  #   }
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :cars
+  #     resources :reservations
+  #   end
+  # end
 
    get '/hello', to: 'application#hello_world'
 end
