5cd0ec6acaf8d01548034aa4c2d1913a3e9522b3
nicholas <nickkiim7@gmail.com>
2023-07-16 22:44:19 +0300

    added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  get 'private/test'
+  get 'private/auth_params'
+  devise_for :users, 
+    path: '', 
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+  namespace :api do
+    namespace :v1 do
+      resources :cars
+      resources :reservations
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-   # route to test your configuration
    get '/hello', to: 'application#hello_world'
 end
